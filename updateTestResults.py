b5480efd3eff6bf0ee69fe9a7a74dc6c301283bf
Michael Dailey <michael.dailey@provartesting.com>
2020-06-11 15:13:37 -0500

    Commenting python script from pipeline

--- updateTestResults.py
+++ updateTestResults.py
@@ -1,13 +1,7 @@
 import xml.etree.ElementTree as ET
-import os
 mytree = ET.parse('ProvarProject/ANT/Results/Overview.html')
-# mytree = ET.parse('Overview.html')
 myroot = mytree.getroot()
 numberOfFailedTests=myroot[1][6][1][1].text
-print(numberOfFailedTests)
 if (int(numberOfFailedTests) > 0):
-    print (numberOfFailedTests + ' Tests Failed')
-    os.environ['AGENT_JOBSTATUS'] = 'Failed'
-    print(os.environ['AGENT_JOBSTATUS'])
+    print (numberOfFailedTests + ' Tests Failed.')
     raise Exception("The pipeline has failed due to at least 1 test case failing.")
-
