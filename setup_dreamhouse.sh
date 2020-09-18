a1e71c5cb9b9e7abae7231ace5e8ca22e166e0e3
Michael Dailey <michael.dailey@provartesting.com>
2020-09-18 12:04:58 -0500

    Added missingt permset name to permset assign step in setup dreamhouse script

--- setup_dreamhouse.sh
+++ setup_dreamhouse.sh
@@ -9,7 +9,7 @@ echo "--- Deploy Dreamhouse to Scratch Org ---"
 sfdx force:source:push -u $1
 ## Assign dreamhouse permission set to the default user
 echo "--- Assign dreamhouse permission set to the default user ---"
-sfdx force:user:permset:assign -n -u $1
+sfdx force:user:permset:assign -n dreamhouse -u $1
 ## Import sample data into Scratch Org
 echo "--- Import sample data into Scratch Org ---"
 sfdx force:data:tree:import -p data/sample-data-plan.json -u $1
