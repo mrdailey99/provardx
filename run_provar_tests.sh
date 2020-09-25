7023a7403675542d657731b0f49ddc1f5a7516dd
Michael Dailey <michael.dailey@provartesting.com>
2020-09-25 12:10:12 -0500

    Added circleci config and necessary scripts to run tests on circleci using provartesting/provar:provardx image

---
+++ run_provar_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd /home
+# Validate ProvarDX property file with plugin
+echo "Validate ProvarDX property file with plugin"
+sfdx provar:validate -p /home/$PROVARDX_PROPERTY_FILE 
+# Compile ProvarDX property file with plugin
+echo "Compile Provar project property file with plugin"
+sfdx provar:compile -p /home/$PROVARDX_PROPERTY_FILE 
+# Run Provar tests on Scratch Org with ProvarDX plugin
+echo "Starting Provar tests on Scratch Org"
+xvfb-run sfdx provar:runtests -p /home/$PROVARDX_PROPERTY_FILE
+echo "---------------Test Run Complete---------------"
