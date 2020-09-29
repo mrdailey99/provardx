aef3cdb77db432868297385edad27318bbd5858e
Michael Dailey <michael.dailey@provartesting.com>
2020-09-29 09:53:44 -0500

    Set to use /home instead of root for permissions in CircleCI job.

--- delete_scratch_org.sh
+++ delete_scratch_org.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /root
+cd /home
 scratch_org_alias=ProvarDX
 echo "Delete Scratch Org from Dev Hub"
 sfdx force:org:delete -u $scratch_org_alias --noprompt
--- run_provar_tests.sh
+++ run_provar_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-cd /root/ProvarDX
+cd /home/ProvarDX
 # Validate ProvarDX property file with plugin
 echo "Validate ProvarDX property file with plugin"
-sfdx provar:validate -p /root/ProvarDX/$PROVARDX_PROPERTY_FILE 
+sfdx provar:validate -p /home/ProvarDX/$PROVARDX_PROPERTY_FILE 
 # Compile ProvarDX property file with plugin
 echo "Compile Provar project property file with plugin"
-sfdx provar:compile -p /root/ProvarDX/$PROVARDX_PROPERTY_FILE 
+sfdx provar:compile -p /home/ProvarDX/$PROVARDX_PROPERTY_FILE 
 # Run Provar tests on Scratch Org with ProvarDX plugin
 echo "Starting Provar tests on Scratch Org"
-xvfb-run sfdx provar:runtests -p /root/ProvarDX/$PROVARDX_PROPERTY_FILE
+xvfb-run sfdx provar:runtests -p /home/ProvarDX/$PROVARDX_PROPERTY_FILE
 echo "---------------Test Run Complete---------------"
