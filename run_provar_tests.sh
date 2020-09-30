1ddd62c4a31f519d706b96ea9d7751927fb4d52f
Michael Dailey <michael.dailey@provartesting.com>
2020-09-30 10:15:23 -0500

    Added 'y' to continue to run test plans with prompt

--- run_provar_tests.sh
+++ run_provar_tests.sh
@@ -8,5 +8,5 @@ echo "Compile Provar project property file with plugin"
 sfdx provar:compile -p /home/$PROVARDX_PROPERTY_FILE 
 # Run Provar tests on Scratch Org with ProvarDX plugin
 echo "Starting Provar tests on Scratch Org"
-xvfb-run sfdx provar:runtests -p /home/$PROVARDX_PROPERTY_FILE
+echo y | xvfb-run sfdx provar:runtests -p /home/$PROVARDX_PROPERTY_FILE
 echo "---------------Test Run Complete---------------"
