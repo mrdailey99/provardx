2c58115332178a77d0ed48b59343bd3ffb6ac730
Michael Dailey <michael.dailey@provartesting.com>
2023-08-22 23:40:44 -0500

    updated to new sf cli in ADO

--- setup_dreamhouse.sh
+++ setup_dreamhouse.sh
@@ -6,10 +6,10 @@ git clone https://github.com/dreamhouseapp/dreamhouse-lwc
 cd dreamhouse-lwc
 ## Deploy Dreamhouse to Scratch Org
 echo "--- Deploy Dreamhouse to Scratch Org ---"
-sfdx project deploy start --target-org $1
+sf project deploy start --target-org $1
 ## Assign dreamhouse permission set to the default user
 echo "--- Assign dreamhouse permission set to the default user ---"
-sfdx org assign permset -n dreamhouse --target-org $1
+sf org assign permset -n dreamhouse --target-org $1
 ## Import sample data into Scratch Org
 echo "--- Import sample data into Scratch Org ---"
-sfdx data import tree -p data/sample-data-plan.json --target-org $1
+sf data import tree -p data/sample-data-plan.json --target-org $1
